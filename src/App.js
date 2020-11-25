69b522d713b2aebcd6f2022897545b0eccef845d
Suryo Adi Nugroho <suryo182@Suryos-MacBook-Pro.local>
2020-11-25 23:22:55 +0700

    Add simple styling and gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,10 @@ class App extends Component {
       .catch((err) => console.log(err.response, '<< error'));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -26,10 +31,10 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monster"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="searh monster"
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
